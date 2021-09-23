ef2a24e8f04943f3ff7efa6571473da1c0f12864
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 15:38:05 +0530

    Update start.py

--- kabeer/pl/start.py
+++ kabeer/pl/start.py
@@ -7,9 +7,10 @@ from pyrogram.types import (
 from kabeer import kabeercmd
 
 START_MESSAGE = (
+        https://telegra.ph/file/e677cf0a08017d8ca0915.jpg
         'Hello there!\n'
         'I can generate session of [pyrogram](https://t.me/pyrogram) and [telethon](https://t.me/TelethonUpdates).\n\n'
-        '**Note:** We are [not](https://telegra.ph/file/e677cf0a08017d8ca0915.jpg) responsible for any harm, And we do not collect your credentials.\n'
+        '**Note:** We are not responsible for any harm, And we do not collect your credentials.\n'
         'By [RhythmOfficial](https://t.me/RhythmOfficial)'
     )
 
