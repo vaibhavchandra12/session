8da3b00ce8b234633af5e72976c59a4858cb3765
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 13:51:44 +0530

    Update start.py

--- kabeer/pl/start.py
+++ kabeer/pl/start.py
@@ -9,11 +9,9 @@ from kabeer import kabeercmd
 START_MESSAGE = (
         'Hello there!\n'
         'I can generate session of [pyrogram](https://t.me/pyrogram) and [telethon](https://t.me/TelethonUpdates).\n\n'
-        '**Note:** We are not responsible for any harm, And we do not collect your credentials.\n'
+        '**Note:** We are [not](https://telegra.ph/file/e677cf0a08017d8ca0915.jpg) responsible for any harm, And we do not collect your credentials.\n'
         'By [RhythmOfficial](https://t.me/RhythmOfficial)'
     )
-
-SIMAGE = (https://telegra.ph/file/e677cf0a08017d8ca0915.jpg)
 
 KEYBOARD = InlineKeyboardMarkup(
     [[InlineKeyboardButton(text='Pyrogram', callback_data='sele_pyrogram')],
@@ -25,6 +23,5 @@ async def start(sessionCli, message):
     await message.reply(
         text=START_MESSAGE,
         reply_markup=KEYBOARD,
-        image=SIMAGE,
-        disable_web_page_preview=True
+        disable_web_page_preview=False
     )
