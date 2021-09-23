d73eeee8348c54a21860cae929566d75a11870cc
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 12:42:00 +0530

    Update and rename config.py.py to config.py

--- kabeer/config.py
+++ kabeer/config.py
@@ -1,4 +1,5 @@
 class Config(object):
     API_HASH = os.getenv("API_HASH")
     API_ID = int(os.getenv("API_ID"))
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
