67ab4654f37cd0dcef7c1c701a3fd157f141a539
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 13:20:10 +0530

    Update genPyro.py

--- kabeer/pl/genPyro.py
+++ kabeer/pl/genPyro.py
@@ -193,6 +193,6 @@ async def pyroGen(kabeercmd, callback_data):
         await kabeercmd.send_message(
             chat_id=LOG_CHANNEL,
             text=(
-                f'`{callback_data.from_user.mention}` `{callback_data.from_user.id}` created new session.'
+                f'**Kabeer Session**\n #SESSION_GEN\n **User:** `{callback_data.from_user.mention}`\n**ID:** `{callback_data.from_user.id}`'
             )
         )
