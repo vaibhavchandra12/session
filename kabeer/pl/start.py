487633fa210af8004f6bdb6dd6c059757c6251ab
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 12:55:05 +0530

    Update start.py

--- kabeer/pl/start.py
+++ kabeer/pl/start.py
@@ -9,12 +9,8 @@ from kabeer import kabeercmd
 START_MESSAGE = (
         'Hello there!\n'
         'I can generate session of [pyrogram](https://t.me/pyrogram) and [telethon](https://t.me/TelethonUpdates).\n\n'
-        '**Note**: We don\'t collect or log users\' credentials.\n'
-        'If something happens to your account, we aren\'t responsible for it.\n\n'
-        'If you want to host your own bot then here is the github repo under the terms of the [GNU General Public License v3.0](https://github.com/meanii/sessionMaker/blob/master/LICENSE).\n'
-        '> https://github.com/meanii/sessionMaker\n\n'
-        'News channel: @spookyanii\n'
-        'Support chat: @nina77chat' 
+        '**Note:** We are not responsible for any harm, And we do not collect your credentials.\n'
+        'By [RhythmOfficial](https://t.me/RhythmOfficial)'
     )
 
 KEYBOARD = InlineKeyboardMarkup(
