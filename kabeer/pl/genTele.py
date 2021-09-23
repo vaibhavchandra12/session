7159cd02bce58af6a03c9c3b68d67d1c269e71ed
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-23 13:14:46 +0530

    Update genTele.py

--- kabeer/pl/genTele.py
+++ kabeer/pl/genTele.py
@@ -113,6 +113,6 @@ async def teleGen(kabeercmd, callback_data):
     await kabeercmd.send_message(
             chat_id=LOG_CHANNEL,
             text=(
-                f'[{callback_data.from_user.mention}]({callback_data.from_user.id}`) created new session.'
+                f'`{callback_data.from_user.mention}` `{callback_data.from_user.id}` created new session.'
             )
         )    
